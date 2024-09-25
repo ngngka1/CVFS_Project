c8fb9c53f98fe4aa9a006f9bed44b7c66f70b8a1
ngngka1 <ngwewe2015@gmail.com>
2024-09-25 21:48:20 +0800

    something

--- src/Disk.java
+++ src/Disk.java
@@ -2,16 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Disk {
-    List<Document> files;
-    List<Directory> directories;
+    Directory rootDirectory;
     // add a stack for undo/redo action
     private int maxSize;
     public int currentSize;
 
     public Disk(int diskSize) {
         maxSize = diskSize;
-        files = new ArrayList<>();
-        directories = new ArrayList<>();
+        rootDirectory = new Directory("");
     }
 
 }
--- src/Document.java
+++ src/Document.java
@@ -1,10 +1,16 @@
+import java.lang.System;
+
 public class Document {
     String name;
     String type;
     String content;
     // remember to add a stack for undo/redo action
     public Document(String name, String type) {
-        this.name = name;
+        if (name != null && name.matches("[A-Za-z0-9]+") && name.length() <= 10) { // fyi: https://www.w3schools.com/python/python_regex.asp
+            this.name = name;
+        } else {
+            System.out.println("Invalid document name!");
+        }
         this.type = type;
         this.content = "";
     }
@@ -12,6 +18,10 @@ public class Document {
     public Document(String name, String type, String content) {
         this(name, type);
         this.content = content;
+    }
+
+    public int size() {
+        return 40 + content.length() * 2;
     }
 
 }
