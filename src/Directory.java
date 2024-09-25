6adcab69675e50af20d91fe25c3bb793f9ecfa85
ngngka1 <ngwewe2015@gmail.com>
2024-09-25 21:48:20 +0800

    something

--- src/Directory.java
+++ src/Directory.java
@@ -1,3 +1,4 @@
+import java.lang.System;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -5,16 +6,26 @@ public class Directory {
     String name; // only digits and letters; at most 10 chars
     List<Document> files; // only of type [txt, java, html, css]
     List<Directory> directories; // sub directories
-    int size;
     // add a stack for undo/redo action
 
     public Directory(String name) {
-        files = new ArrayList<>();
-        directories = new ArrayList<>();
-        if (name != null && name.matches("[A-Za-z0-9]+")) { // fyi: https://www.w3schools.com/python/python_regex.asp
+        if (name != null && name.matches("[A-Za-z0-9]+") && name.length() <= 10) { // fyi: https://www.w3schools.com/python/python_regex.asp
             this.name = name;
         } else {
-            // throw some exception
+            System.out.println("Invalid directory name!");
         }
+        files = new ArrayList<>();
+        directories = new ArrayList<>();
+    }
+
+    public int size() {
+        int totalSize = 0;
+        for (Document file : files) {
+            totalSize += file.size();
+        }
+        for (Directory directory : directories) {
+            totalSize += directory.size();
+        }
+        return totalSize;
     }
 }
--- src/System.java
+++ src/System.java
@@ -5,25 +5,53 @@ public class System {
     private static System systemInstance; // private so singleton instance doesnt get directly accessed
     List<Disk> disks;
     int selectedDiskIndex;
-    String workingDirectory; // expected value example: /dir1/dir2
+    String workingDirectoryAbsolutePath; // expected value example: /dir1/dir2
+    Directory workingDirectory;
 
     private System() {
         selectedDiskIndex = 0;
         disks = new ArrayList<>();
+        workingDirectoryAbsolutePath = "/";
+        workingDirectory = null;
     }
-    public static System createInstance() {
+    private static void createInstance() {
         if (System.systemInstance == null) {
             System.systemInstance = new System();
         }
-        return System.systemInstance;
     }
     public static System getInstance() {
-        if (System.systemInstance != null) {
-            return System.systemInstance;
-        } else {
+        if (System.systemInstance == null) {
+            createInstance();
+        }
+        return System.systemInstance;
+    }
+
+    // maybe change the function name later
+    public static Directory traverseDirectoriesRecursive(String targetedPath, Directory workingDirectory) {
+        // expected cases for targetedPath:
+        // "/dir1": invalid input (this function cant handle absolute path)
+        // "./dir1": valid input (relative path)
+        // "dir1": valid input (relative path)
+        if (targetedPath.isEmpty()) {
+            return workingDirectory;
+        }
+        if (targetedPath.startsWith("/")) {
+
             return null;
         }
+        if (!targetedPath.endsWith("/")) {
+            targetedPath += "/";
+        }
+        int delimiterIndex = targetedPath.indexOf('/');
+        String targetedDirectoryName = targetedPath.substring(0, delimiterIndex);
+        for (Directory directory : workingDirectory.directories) {
+            if (directory.name.equals(targetedDirectoryName)) {
+                return traverseDirectoriesRecursive(targetedPath.substring(delimiterIndex + 1), directory);
+            }
+        }
+        return null;
     }
+
     public static Disk getWorkingDisk() {
         return systemInstance.disks.get(systemInstance.selectedDiskIndex);
     }
@@ -31,7 +59,10 @@ public class System {
     // WIP
     public static Directory getWorkingDirectory() {
         final Disk workingDisk = getWorkingDisk();
-        return workingDisk.directories.get(-1);
+        if (systemInstance.workingDirectoryAbsolutePath.equals("/")) {
+            return workingDisk.rootDirectory;
+        }
+        return traverseDirectoriesRecursive(systemInstance.workingDirectoryAbsolutePath, workingDisk.rootDirectory);
     }
 
     public static void newDisk(int diskSize) {
@@ -46,7 +77,7 @@ public class System {
         getWorkingDirectory().files.add(new Document(docName, doctype, docContent));
     }
 
-    public void deleteFile(String fileName) {
+    public static void deleteFile(String fileName) {
         List<Document> files = getWorkingDirectory().files;
         for (int i = 0; i < files.size(); i++) {
             if (files.get(i).name.equals(fileName)) {
@@ -57,7 +88,7 @@ public class System {
 //        throw whatever exception idk might do this later
     }
 
-    public void renameFile(String oldFileName, String newFileName) {
+    public static void renameFile(String oldFileName, String newFileName) {
         List<Document> files = getWorkingDirectory().files;
         for (Document file : files) {
             if (file.name.equals(oldFileName)) {
@@ -67,6 +98,34 @@ public class System {
         }
 //        throw whatever exception idk might do this later
     }
+
+    public static void changeDirectory(String newDirectoryName) {
+        while (newDirectoryName.startsWith("..")) {
+            navigateParentDirectory();
+            int delimiterIndex = newDirectoryName.indexOf('/');
+            newDirectoryName = newDirectoryName.substring(delimiterIndex + 1);
+        }
+        Directory newDirectory = traverseDirectoriesRecursive(newDirectoryName, systemInstance.workingDirectory);
+        if (newDirectory != null) {
+            systemInstance.workingDirectory = newDirectory;
+            systemInstance.workingDirectoryAbsolutePath += "/" + newDirectoryName;
+        } else {
+            java.lang.System.out.println("Targeted folder cannot be found!");
+        }
+    }
+
+    private static void navigateParentDirectory() {
+        if (systemInstance.workingDirectoryAbsolutePath.equals("/")) {
+            java.lang.System.out.println("Base directory is already reached!");
+            return;
+        }
+        int delimiterIndex = systemInstance.workingDirectoryAbsolutePath.lastIndexOf('/');
+        systemInstance.workingDirectoryAbsolutePath = systemInstance.workingDirectoryAbsolutePath.substring(0, delimiterIndex);
+    }
+
+//    public static List<> listFiles() {
+//
+//    }
 }
 
 //main {
