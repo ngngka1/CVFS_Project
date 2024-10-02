788ce12f177e081ec68861f5d0b0f298d9e7e45b
ngngka1 <ngwewe2015@gmail.com>
2024-10-02 23:08:25 +0800

    improved error handling & renamed a class attribute

--- src/Directory.java
+++ src/Directory.java
@@ -1,10 +1,9 @@
-import java.lang.System;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Directory {
     String name; // only digits and letters; at most 10 chars
-    List<Document> files; // only of type [txt, java, html, css]
+    List<Document> documents; // only of type [txt, java, html, css]
     List<Directory> directories; // sub directories
     // add a stack for undo/redo action
 
@@ -12,9 +11,9 @@ public class Directory {
         if ((isRootDirectory) || (name != null && name.matches("[A-Za-z0-9]+") && name.length() <= 10)) { // fyi: https://www.w3schools.com/python/python_regex.asp
             this.name = name;
         } else {
-            System.out.println("Invalid directory name!");
+            throw new IllegalArgumentException("Invalid directory name!");
         }
-        files = new ArrayList<>();
+        documents = new ArrayList<>();
         directories = new ArrayList<>();
     }
 
@@ -23,9 +22,9 @@ public class Directory {
     }
 
     public int size() {
-        int totalSize = 0;
-        for (Document file : files) {
-            totalSize += file.size();
+        int totalSize = System.defaultDirectorySize;
+        for (Document document : documents) {
+            totalSize += document.size();
         }
         for (Directory directory : directories) {
             totalSize += directory.size();
