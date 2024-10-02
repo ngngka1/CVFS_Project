4f2bb91e8b1a05a500309bf7b226efe5754374c4
ngngka1 <ngwewe2015@gmail.com>
2024-10-02 23:07:46 +0800

    minor changes for data encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System systemInstance = System.getInstance();
-        while (systemInstance.isRunning) {
+        final System systemInstance = System.getInstance();
+        while (systemInstance.isRunning()) {
             CLI.renderCLI();
         }
     }
 }
+//newDisk 999
+//newDir test1
+//changeDir $:test1
+//newDir test2
+//changeDir $:test2
+//changeDir $:..:..
+//changeDir $:test1:..:test1:..:test1:test2:..:..:test1
